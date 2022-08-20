3a19085dc353b791ae4abbc8740cabf6c00585cf
Soroush MasoomBabaei <68003197+SoroushMB@users.noreply.github.com>
2022-08-20 13:37:51 +0430

    Update AnimeDownloader.py

--- AnimeDownloader.py
+++ AnimeDownloader.py
@@ -13,5 +13,5 @@ BeautifulSoap(get_html_doc(url),'lxml')
 def main_screep(url:str):
     'anime_info_body_bg'
     suop = bs4_data(url)
-    # data = (suop.find('div',{'class':'play-video'}).find('iframe')['src'].split('/')[-1].split('¿))
+    # data = (suop.find('div',{'class':'play-video'}).find('iframe')['src'].split('/')[-1].split('?))
     data = (suop.find('div',{'class':}))
